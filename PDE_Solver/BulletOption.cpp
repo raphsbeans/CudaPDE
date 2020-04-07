e87e4de18305b497689d65e1e711de422f4c0d44
Raphael Feijao <raphaelfeijao1@gmail.com>
2020-04-07 19:03:11 -0400

    Added some new functions to BulletOption

--- PDE_Solver/BulletOption.cpp
+++ PDE_Solver/BulletOption.cpp
@@ -1,10 +1,22 @@
 #include "BulletOption.h"
+#include  <math.h>
 
-BulletOption::BulletOption(float Strike, size_t daysToMaturity, float P1, float P2, float B, size_t* pre_schedule)
-    :Strike(Strike), daysToMaturity(daysToMaturity), P1(P1), P2(P2), B(B), pre_schedule(pre_schedule){}
+BulletOption::BulletOption (float Strike, size_t daysToMaturity, float S0, float sMin, float sMax, float P1, float P2, float B,
+    size_t* pre_schedule, size_t spotGridSize, size_t timeGridSize, size_t stateSize)
+    :Strike(Strike), daysToMaturity(daysToMaturity), P1(P1), P2(P2), B(B), pre_schedule(pre_schedule), 
+    spotGridSize(spotGridSize), timeGridSize(timeGridSize), stateSize(stateSize), sMin(sMin), sMax(sMax), S0(S0){
+    dx = (log(sMax) - log(sMin)) / spotGridSize;
+    dt = daysToMaturity / timeGridSize;
+}
 
 void BulletOption::initPayoff(float* payoff) {
-
+    double S;
+    for (size_t j = 0; j < stateSize; j++) {
+        for (size_t i = 0; i < spotGridSize; i++) {
+            S = exp(sMin + (double)i * dx);
+            payoff[i + j * spotGridSize] = (S - Strike > 0 &&  j > P1 && j < P2) ? (S - Strike) : 0;
+        }
+    }
 }
 
 void BulletOption::interStep(float* payoff, size_t timeIdx) {
@@ -12,6 +24,20 @@ void BulletOption::interStep(float* payoff, size_t timeIdx) {
 }
 
 float BulletOption::applySolution(float* sol) {
+    size_t left = (size_t)floor((S0 - sMin) / dx);
+    size_t right = left + 1;
+    return sol[left] + sol[right] * (S0 - sol[left]) / (sol[right] - sol[left]);
 
 }
 
+size_t BulletOption::getGridSizes(size_t& spotGridSize, size_t& timeGridSize, size_t& stateSize) {
+    spotGridSize = this->spotGridSize;
+    timeGridSize = this->timeGridSize;
+    stateSize = this->stateSize;
+    return 1;
+}
+
+void BulletOption::getSpotRange(float& sMin, float& sMax) {
+    sMin = this->sMin;
+    sMax = this->sMax;
+}
--- PDE_Solver/BulletOption.h
+++ PDE_Solver/BulletOption.h
@@ -2,18 +2,30 @@
 #include "Payoff.h"
 class BulletOption : public Payoff {
 public:
-	BulletOption(float Strike, size_t daysToMaturity, float P1, float P2, float B, size_t * pre_schedule);
+	BulletOption(float Strike, size_t daysToMaturity, float S0, float sMin, float sMax, float P1, float P2, float B, 
+		size_t * pre_schedule, size_t spotGridSize, size_t timeGridSize, size_t stateSize);
 	void initPayoff(float* payoff);
 	void interStep(float* payoff, size_t timeIdx);
 	float applySolution(float* sol);
+	void getSpotRange(float& sMin, float& sMax);
+	size_t getGridSizes(size_t& spotGridSize, size_t& timeGridSize, size_t& stateSize);
 
 private:
-	size_t daysToMaturity = 0;
-	size_t ind_counter = 0;
-	float Strike = 0.0f;
-	float P1 = 0.0f;
-	float P2 = 0.0f;
-	float B = 0.0;
+	size_t daysToMaturity = 0; //Only working days
+	size_t spotGridSize = 0; 
+	size_t timeGridSize = 0; 
+	size_t stateSize = 0;
+
+	float S0 = 0.0f; //Initial Price
+	float Strike = 0.0f; //Strike
+	float P1 = 0.0f; //Minimum Times heating B
+	float P2 = 0.0f; //Maximum Times heating B
+	float B = 0.0f; //The limit 
+	float sMin = 0.0f; //Minimum value in the spot grid
+	float sMax = 0.0f; //Maximum value in the spot grid
+	float dx = 0.0f; //infinitesimal value for the spot grid
+	float dt = 0.0f; //infinitesimal -> grid time
+
 	size_t* pre_schedule;
 };
 
